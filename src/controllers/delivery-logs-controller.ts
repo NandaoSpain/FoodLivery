34cb8bb910e3c39cf86a7009315baca22da00446
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-01-29 17:16:19 -0300

    delivered orders

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -7,47 +7,69 @@ class DeliveryLogsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       delivery_id: z.string().uuid(),
-      description: z.string()
+      description: z.string(),
     });
 
-    const { delivery_id, description } = bodySchema.parse(request.body)
+    const { delivery_id, description } = bodySchema.parse(request.body);
 
-    const delivery = await prisma.delivery.findUnique({ where: { id: delivery_id } })
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
 
     if (!delivery) {
-      throw new AppError("delivery not found", 404)
+      throw new AppError("delivery not found", 404);
     }
 
     if (delivery.status === "processing") {
-      throw new AppError("Cannot create delivery log for processing delivery", 400)
+      throw new AppError(
+        "Cannot create delivery log for processing delivery",
+        400
+      );
+    }
+
+    if (delivery.status === "delivered") {
+      throw new AppError(
+        "This order has already been delivered",
+        400
+      );
     }
 
     await prisma.deliveryLog.create({
       data: {
         deliveryId: delivery_id,
-        description
-      }
-    })
+        description,
+      },
+    });
 
     return response.status(201).json();
   }
 
   async show(request: Request, response: Response) {
     const paramsSchema = z.object({
-      delivery_id: z.string().uuid()
-    })
+      delivery_id: z.string().uuid(),
+    });
 
-    const { delivery_id } = paramsSchema.parse(request.params)
+    const { delivery_id } = paramsSchema.parse(request.params);
 
-    const delivery = await prisma.delivery.findUnique({ where: { id: delivery_id }})
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+      include: {
+        logs: {
+          select: { description: true, createdAt: true, updatedAt: true },
+        },
+        user: { select: { name: true, email: true } },
+      },
+    });
 
-    if (request.user?.role === "customer" && request.user.id !== delivery?.userId) {
-      throw new AppError("unauthorized", 403)
+    if (
+      request.user?.role === "customer" &&
+      request.user.id !== delivery?.userId
+    ) {
+      throw new AppError("unauthorized", 403);
     }
-    
-    return response.json(delivery)
-  }
 
+    return response.json(delivery);
+  }
 }
 
 export { DeliveryLogsController };
