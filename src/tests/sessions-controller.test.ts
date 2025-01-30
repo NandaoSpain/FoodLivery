6a3a7fb96137ca332100384b6104b164d94ac74e
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-01-30 15:39:38 -0300

    Update sessions-controller.test.ts

--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -4,14 +4,14 @@ import { prisma } from "@/database/prisma";
 
 describe("SessionsController", () => {
   let user_id: string;
-  
+  afterAll(async () => await prisma.user.delete({ where: { id: user_id } }));
   it("should authenticate and get access token", async () => {
     const userResponse = await request(app).post("/users").send({
       name: "Auth Test User",
       email: "auth_test_user@example.com",
       password: "password123",
-    });
-
+    });    
+    
     user_id = userResponse.body.id;
 
     const sessionResponse = await request(app).post("/sessions").send({
