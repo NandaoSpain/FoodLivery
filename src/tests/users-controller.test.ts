95f32208942bc0999d5140eb81de88cb30f42672
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-01-30 15:03:47 -0300

    test user email already exists with jest

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,18 +1,34 @@
 import request from "supertest";
 import { app } from "@/app";
+import { prisma } from "@/database/prisma";
 
 describe("usersController", () => {
+  let user_id: string;
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
   it("should create a new user successfully", async () => {
-    const response = await request(app)
-     .post("/users")
-     .send({
-        name: "Test User",
-        email: "testuser@example.com",
-        password: "password123",
-      });
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
-    expect(response.body.name).toBe("Test User");  
+    expect(response.body.name).toBe("Test User");
+    user_id = response.body.id;
   });
+
+  it("should throw an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("User email already exists");
+  })
 });
